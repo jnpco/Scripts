ecfc8fa18a5d7342433292d27a15e5b29fa7ef52
JOR <jnpco.98@gmail.com>
2019-04-13 04:33:36 +1000

    Added file extensions for file organizer.

---
+++ file_organizer.py
@@ -0,0 +1,12 @@
+import os
+
+config = {
+    'DOCUMENTS': ['.csv', '.txt', '.doc', '.docx', '.ppt', '.pptx', '.rtf', '.txt', '.vsd', '.xls', '.xlsx'],
+    'IMAGES': ['.bmp', '.gif', '.ico', '.jpeg', '.jpg', '.png', '.svg', '.tiff', '.webp'],
+    'VIDEOS': ['.avi', '.flv', '.mkv', '.mov', '.mpeg', '.mpg', '.mp4', '.ogv', '.vob', '.webm', '.wmv', '.3gp'],
+    'AUDIO': ['.aac', '.aa', '.mp3', '.m4a', '.m4b', '.m4p', '.oga', '.wav', '.weba', '.mwa'],
+    'COMPRESSED': ['.bz', '.bz2', '.rar', '.zip', '.7z'],
+    'EBOOKS': ['.epub', '.mobi', '.pdf']
+}
+
+# option include subdirectory -warning
