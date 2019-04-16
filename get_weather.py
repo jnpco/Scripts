e3daab9a5630b3921df1aea723d5f1be28c2c99e
JOR <jnpco.98@gmail.com>
2019-04-17 01:29:06 +1000

    Renamed summary to three day summary. Change test for loop. Added Wind and Daily summary.

--- get_weather.py
+++ get_weather.py
@@ -18,23 +18,25 @@ def get_weather(location):
 
     forecast_rows = {
         # 1-3, 4-7, 7-10 days summaries
-        'summary': thead.select('tr.b-forecast__table-description.b-forecast__hide-for-small.days-summaries span.phrase'),
+        'three_day_summary': thead.select('tr.b-forecast__table-description.b-forecast__hide-for-small.days-summaries span.phrase'),
         # Odd idx is the day of the week, followed by date
         'days': thead.select('tr.b-forecast__table-days.js-forecast-header.js-daynames span.b-forecast__table-days-name,span.b-forecast__table-days-date'),
 
         # Same no. of columns
         # Morning Evening Night
         'period': thead.select('tr.b-forecast__table-time.js-daytimes span.b-forecast__table-value'),
+        # Wind km/h
+        'wind': tbody.select('tr.b-forecast__table-wind div.b-forecast__table-wind-container text.wind-icon-val'),
+        # Summary clear, cloudy etc.
+        'summary': tbody.select('tr.b-forecast__table-summary td.b-forecast__table-day-end div.b-forecast__text-limit')
     }
     # if not am, arrange data set
 
-    for forecast in forecast_rows['summary']:
-        print(forecast.text)
-    for forecast in forecast_rows['days']:
-        print(forecast.text)
-
-    for forecast in forecast_rows['period']:
-        print(forecast.text)
+    for row in forecast_rows:
+        print(row + ' col no. --> ' + str(len(forecast_rows[row])) + '\n')
+        for item in forecast_rows[row]:
+            print(item.text)
+        print('------------------------------------------------------------')
 
 
 get_weather('sydney')
