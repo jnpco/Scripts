f81b5fe7fc021ec9567c617cc1a6bf7ade89c0c5
JOR <jnpco.98@gmail.com>
2019-04-18 12:43:14 +1000

    Refactored vid_duration.py

--- vid_duration.py
+++ vid_duration.py
@@ -6,21 +6,32 @@ from pymediainfo import MediaInfo
 # Install pymediainfo using pip.
 # Download the mediainfo dll, or add to path.
 # Run script to check combined duration of all videos in a directory and it's subdirectories.
+# TODO Add option to check just one file
+# TODO Add option for non-recursive check
 
-duration = 0
-for arg in sys.argv[1:]:
-    for root, dirs, files in os.walk(arg):
-        for file in files:
-            vid_info = MediaInfo.parse(os.path.join(root, file))
-            for track in vid_info.tracks:
-                if track.track_type == 'Video':
-                    duration += track.duration
-                    print(file, "\t Duration: {} secs".format(
-                        track.duration / 1000))
 
-mins, secs = divmod(duration / 1000, 60)
-hours, mins = divmod(mins, 60)
-days, hours = divmod(hours, 24)
+def get_vid_duration_dirs(dirs):
+    total_duration = 0
+    for dir in dirs:
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                vid_info = MediaInfo.parse(os.path.join(root, file))
+                for track in vid_info.tracks:
+                    if track.track_type == 'Video':
+                        total_duration += track.duration
+                        print(file, "\t Duration: {} secs".format(
+                            track.duration / 1000))
+
+    mins, secs = divmod(total_duration / 1000, 60)
+    hours, mins = divmod(mins, 60)
+    days, hours = divmod(hours, 24)
+
+    return (days, hours, mins, secs)
+
+
+duration = get_vid_duration_dirs(sys.argv[1:])
+
+days, hours, mins, secs = duration
 
 print('\nTotal duration (D:H:M:S): {:0>2}:{:0>2}:{:0>2}:{:0>2}'.format(int(days),
                                                                        int(hours), int(mins), int(secs)))
